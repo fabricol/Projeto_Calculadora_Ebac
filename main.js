108cbc950c62b69c8d9d518c3a462bd4b097c908
fabricol <fabricioctnmiranda@gmail.com>
2024-01-27 18:41:54 -0300

    atualização para a lista telefônica

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 const form = document.getElementById('formAtividade');
-const imgAprovado = '<img src="./img/approved.png" alt="aprovado" height="50" width="50"/>'
-const imgReprovado = '<img src="./img/disapproved.png" alt="reprovado" height="50" width="50"/>'
-const imgFeliz = '<img src="./img/festa.png" alt="reprovado" height="50" width="50"/>'
-const imgTriste = '<img src="./img/sad.png" alt="reprovado" height="50" width="50"/>'
 const atividade = [];
 const notas = []
-const notaMinima = parseFloat(prompt('Digite a nota mínima:'))
 let linhas =''
 
 form.addEventListener('submit', function (e) {
@@ -29,7 +24,6 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgFeliz : imgTriste }</td>`;
         linha += `<tr>`;
 
     linhas += linha
@@ -45,17 +39,6 @@ function atualizaTabela() {
 }
 
 function AtualizaMediaFinal() {
-    mediaFinal = CalculaMediaFinal();
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
     document.getElementById('media-final-resposta').innerHTML = mediaFinal >= 7 ? imgAprovado : imgReprovado;   
-}
-
-function CalculaMediaFinal() {
-    let somaDasNotas = 0;
-
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i]
-    }
-    return somaDasNotas/notas.length
-    
 }
